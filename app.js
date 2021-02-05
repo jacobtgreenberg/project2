c3d8e71cf5013d83ae96e965af2ecb18f026288a
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-05 16:14:39 -0500

    created circles

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("hello")
+//Create a 7 x 6 grid
+//want to differentiate by column
+//want a hover feature so player knows where they are dropping
+//populate bottom available hole of column
+//alternate color----> function that takes color parameter and alters css.class
+
+$(() => {
+
+for(let i = 1;i <= 6; i++){
+    const $row = $('<div>')
+    $('body').append($row)
+    for(let j = 1;j <= 7;j++){
+        const $circle = $('<div>').addClass('circle')
+        $row.append($circle)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
