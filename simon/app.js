ceb35ca0a38392a28857ff9074d046e97d6ae7ee
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-05 20:48:57 -0500

    got rid of e target in toggle functions

--- simon/app.js
+++ simon/app.js
@@ -8,31 +8,48 @@
 
 $(()  => {
 
-function toggleRed(e) {
-    $(e.target).toggleClass('redLit')
+function lightRandomColor() {
+    const randomIndex = Math.ceil(Math.random() * 4)
+    if(randomIndex === 1){
+        toggleRed
+    }
+}
+
+lightRandomColor()
+
+// function toggleRed(e) {
+//     $(e.target).toggleClass('redLit')
+//      setTimeout(() => {
+//          $(e.target).toggleClass('redLit')
+//      }, 400)
+// }
+
+function toggleRed() {
+    $('.red').toggleClass('redLit')
      setTimeout(() => {
-         $(e.target).toggleClass('redLit')
+         $('.red').toggleClass('redLit')
      }, 400)
 }
 
-function toggleBlue(e) {
-    $(e.target).toggleClass('blueLit')
+
+function toggleBlue() {
+    $('.blue').toggleClass('blueLit')
     setTimeout(() => {
-        $(e.target).toggleClass('blueLit')
+        $('.blue').toggleClass('blueLit')
     }, 400)
 }
 
 function toggleYellow(e) {
-    $(e.target).toggleClass('yellowLit')
+    $('.yellow').toggleClass('yellowLit')
     setTimeout(() => {
-        $(e.target).toggleClass('yellowLit')
+        $('.yellow').toggleClass('yellowLit')
     }, 400)
 }
 
 function toggleGreen(e) {
-    $(e.target).toggleClass('greenLit')
+    $('.green').toggleClass('greenLit')
     setTimeout(() => {
-        $(e.target).toggleClass('greenLit')
+        $('.green').toggleClass('greenLit')
     }, 400)
 }
 
@@ -43,34 +60,6 @@ $('.green').on('click', toggleGreen)
 
 
 
-//  $('.red').on('click', (e) => {
-//      $(e.target).toggleClass('redLit')
-//      setTimeout(() => {
-//          $(e.target).toggleClass('redLit')
-//      }, 400)
-//  })
-
-//  $('.blue').on('click', (e) => {
-//     $(e.target).toggleClass('blueLit')
-//     setTimeout(() => {
-//         $(e.target).toggleClass('blueLit')
-//     }, 400)
-// })
-
-// $('.yellow').on('click', (e) => {
-//     $(e.target).toggleClass('yellowLit')
-//     setTimeout(() => {
-//         $(e.target).toggleClass('yellowLit')
-//     }, 400)
-// })
-
-
-// $('.green').on('click', (e) => {
-//     $(e.target).toggleClass('greenLit')
-//     setTimeout(() => {
-//         $(e.target).toggleClass('greenLit')
-//     }, 400)
-// })
 
 
 
