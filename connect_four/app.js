cb611b829cbb1b026a0e43bd5bbd8ddda736ed16
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-05 23:44:54 -0500

    cleaning up local

--- connect_four/app.js
+++ connect_four/app.js
@@ -1,5 +1,5 @@
 //Create a 7 x 6 grid
-//want to differentiate by column
+//want to differentiate by column// read online better to id each circle/div and target ahead
 //want a hover feature so player knows where they are dropping
 //populate bottom available hole of column
 //alternate color----> function that takes color parameter and alters css.class
@@ -11,6 +11,19 @@ for(let i = 1;i <= 7; i++){
     $('#container').append($column)
     for(let j = 1;j <= 6;j++){
         const $circle = $('<div>').addClass('circle')
+
+        $column.on('click', (e)=> {
+
+            for(children of $(e.target)){
+                console.log(children, i, j)
+            }
+        
+        })
+
+
+        $circle.on('click', () => {
+            console.log('i is '+ i +' j is ' +j)
+        })
         $column.append($circle)
     }
 
