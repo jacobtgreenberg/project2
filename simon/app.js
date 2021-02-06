38a700e0b0a2777272fe3c4d46aa409651da6e8d
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-05 21:48:11 -0500

    added loop through computer array function

--- simon/app.js
+++ simon/app.js
@@ -2,21 +2,51 @@
 ///Need to generate random sequence --->
         ///need to assign value to each key to access
         /// need to store previous sequence and append to that
-///Need to check player entry against computer entry
+
+///Need to check player entry against computer entry, need to keep count of player entry
+/// need level counter to go up by milliseconds
+
+///need to make function that loops through computer array and then calls random function
+//// startInterval function would then call THIS function, not random interval
+
+///Need to empty player array but keep computer array
+///need to set counter that only loops through computer array as many clicks as the player has done
+
+$(()  => {
+
+const computerArray = [1, 1, 2, 3, 1, 4, 1, 2];
+const playerArray = [];
+
+let level = 1005;
+
+function startInterval() {
+    const interval = setInterval(lightRandomColor, 1000 )
+    setTimeout(() => {
+        clearInterval(interval)
+    }, level)
+}
 
 
 
-// $(()  => {
+function loopThroughComputerArray() {
+    let counter = 0;
+    
+    setInterval( () =>  {
+        if(computerArray[counter] === 1){
+            toggleRed()
+        }else if(computerArray[counter] === 2){
+            toggleBlue()
+        }else if(computerArray[counter] === 3){
+            toggleYellow()
+        }else if(computerArray[counter] === 4){
+            toggleGreen()
+        }
+        counter++
+    }   
+    , 1005)
+}
 
-
-//     setInterval(()=> {
-
-//         lightRandomColor()
-        
-        
-//         }, 1000 )
-
-
+loopThroughComputerArray()
 
 function lightRandomColor() {
     const randomIndex = Math.ceil(Math.random() * 4)
@@ -26,12 +56,11 @@ function lightRandomColor() {
         toggleBlue()
     }else if(randomIndex === 3){
         toggleYellow()
-    }else if(randomIndex === 4)[
+    }else if(randomIndex === 4){
         toggleGreen()
-    ]
+    }
 }
 
-// lightRandomColor() 
 
 
 // When passed into click event is called
