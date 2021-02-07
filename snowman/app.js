e9712c4760bce3a17273fce24e6da603ba3f27bf
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-07 10:20:14 -0500

    got basic gameplay going

---
+++ snowman/app.js
@@ -0,0 +1,62 @@
+/// Need word bank
+///Need to choose word
+/// Need to generate divs populated by letters of word
+///Need to display alphabet
+///Need to be able to click on each alphabet element
+    ////Go through chosen word and see if letter is there
+    ////If letter is there
+
+$(() => {
+
+const word = ["s", "w" , "o", "r" , "d"]
+const alphabet = ["a", "b", "c", "d", "e", "f", "g","h","i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+///click on target
+///get targets value, text
+//if text value is in word one change, if not, another
+
+function checkWord(e){
+    const $chosen = $(e.target).text()
+    if(word.includes($chosen)){
+        $(e.target).remove();
+        for(child of $('#container').children()){
+            if($(child).text() == $chosen){
+                $(child).css('background-color','blue')
+            }
+            }
+    }else{
+        $(e.target).css('background-color','red')
+    }
+
+}
+
+for(letter of alphabet ){
+    const $div = $('<div>').text(letter).addClass('omega')
+    $('#alpha').append($div)
+    $div.on('click', checkWord)
+}
+
+for(letter of word){
+    const $div = $('<div>').text(letter).addClass('current')
+    $('#container').append($div)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
