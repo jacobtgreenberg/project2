446a264cf8865ca7d17163b07433b5b5d6945cb3
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-07 12:47:19 -0500

    setting up to remove words from word bank

---
+++ snowman/scratch4.js
@@ -0,0 +1,117 @@
+
+    $(() => {
+
+
+        const wordBank = [
+            ["f", "i", "s", "h"],
+            ["s","w","o","r","d"],
+            ["o","c","t","o","p","u","s"],
+            ["v","e","g","e","t","a","b","l","e"],
+            ["p","r","o","g","r","a","m","m","e","r"]
+        ]
+
+        let word = []
+        const alphabet = ["a", "b", "c", "d", "e", "f", "g","h","i", "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        
+        
+        let strikes = 0;
+        let lettersCorrect = 0;
+        let $chosenWord;
+        
+        function check(){
+            let argh = wordBank.indexOf($chosenWord)
+            console.log(argh)
+        }
+
+        function pickWord() {
+            const $randomIndex = Math.floor(Math.random() * wordBank.length)
+            $chosenWord = wordBank[$randomIndex]
+           
+             for(letter of $chosenWord){
+                 word.push(letter)
+             }
+             
+             console.log($chosenWord)
+        }
+
+        function setAlphabet() {
+            for(letter of alphabet ){
+                const $div = $('<div>').text(letter).addClass('omega')
+                $('#alpha').append($div)
+                $div.on('click', checkWord)
+            }
+        }
+
+        function setWord() {
+            for(letter of word){
+                const $div = $('<div>').text(letter).addClass('current')
+                $('#container').append($div)
+            }
+        }
+
+        function checkWord(e){
+            const $chosen = $(e.target).text()
+            if(word.includes($chosen)){
+                $(e.target).remove();
+                letterCorrect(e)
+            }else{
+                $(e.target).remove();
+                strikes++;
+                letterIncorrect()
+            }   
+        }
+
+        function letterCorrect(e) {
+            for(child of $('#container').children()){
+                if($(child).text() == $(e.target).text()){
+                    $(child).css('color','black')
+                    lettersCorrect++
+                    if(lettersCorrect == word.length){
+                        $('#chances').text("You got the word!")
+                        playAgain()
+                    }
+                }
+            }
+        }
+
+        function letterIncorrect() {
+            if(strikes == 1){
+                $('#chances').text(strikes + " strike")
+            }else if(strikes == 6){
+                $('#chances').text("Ya lose.")
+                playAgain()
+            }
+            else{
+                $('#chances').text(strikes + " strikes")
+            }
+        }
+
+
+        function playAgain() {
+            const $div = $('<div>').addClass('replay')
+            $div.text('Play Again')
+            $('body').append($div)
+            for(child of $('#container').children()){
+                $(child).css('color','black')
+            }
+            $div.on('click' , (e) => {
+                $('#alpha').empty()
+                $('#container').empty()
+                $(e.target).remove()
+                word = []
+                setAlphabet()
+                pickWord()
+                setWord()
+                lettersCorrect = 0;
+                strikes = 0;
+                $('#chances').text(strikes + " strikes")
+            })
+        }
+        
+    setAlphabet()
+    pickWord()
+    setWord()
+   
+
+        
+    })
