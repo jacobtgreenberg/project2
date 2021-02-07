b39bef6e1b3c1b5b18a9158cec44f0b691280d24
Jacob Greenberg <jacobgreenberg@Jacobs-MacBook-Pro.local>
2021-02-06 19:56:45 -0500

    cleaned up code

--- simon/scratch.js
+++ simon/scratch.js
@@ -2,42 +2,51 @@ $(()  => {
 
     const computerArray = [];
     let playerArray = [];
-    
-    const ping = ('#ping')
-    
-    
-    
-    //// determines when to clear interval on startInterval function, may be obsolete
+    //// will be applied to loopThroughCA, need to limit sounds duration
     let level = 3005;
-    
-    //if clickCounter === computerArray.length player turn over --- need to empty array, if playerArray[clickCounter] === computerArray[clickCounter] gets a point, if != game over
     let clickCounter = 0
-    
-    //keep track of players score
     let score = 0
     
-    ///gets random color to go and start, may be obsolete, may need to house
-    // function startInterval() {
-    //     const interval = setInterval(lightRandomColor, 1000 )
-    //     setTimeout(() => {
-    //         clearInterval(interval)
-    //     }, level)
-    // }
+    function toggleC(call, color, sound){
+        $(call).toggleClass(color + 'Lit')
+         setTimeout(() => {
+             $(call).toggleClass(color + 'Lit')
+         }, 400)
+         $(sound).get(0).play()
+    }
     
-    // startInterval()
-    
+    function lightRandomColor() {
+        const randomIndex = Math.ceil(Math.random() * 4)
+        if(randomIndex === 1){
+            toggleC('.red', 'red', '#ding')
+            computerArray.push(1)
+            console.log("computer Array" + computerArray)
+        }else if(randomIndex === 2){
+            toggleC('.blue', 'blue', '#bleep')
+            computerArray.push(2)
+            console.log(computerArray)
+        }else if(randomIndex === 3){
+            toggleC('.yellow', 'yellow', '#ping')
+            computerArray.push(3)
+            console.log(computerArray)
+        }else if(randomIndex === 4){
+            toggleC('.green', 'green', '#beep')
+            computerArray.push(4)
+            console.log(computerArray)
+        }
+    }
+
     function loopThroughComputerArray() {
         let counter = 0;
-        
         const start = setInterval( () =>  {
             if(computerArray[counter] === 1){
-                toggleRedC()
+                toggleC('.red', 'red', '#ding')
             }else if(computerArray[counter] === 2){
-                toggleBlueC()
+                toggleC('.blue', 'blue', '#bleep')
             }else if(computerArray[counter] === 3){
-                toggleYellowC()
+                toggleC('.yellow', 'yellow', '#ping')
             }else if(computerArray[counter] === 4){
-                toggleGreenC()
+                toggleC('.green', 'green', '#beep')
             }else{
                 lightRandomColor();
                 setTimeout(() => {clearInterval(start)},100 )
@@ -47,84 +56,7 @@ $(()  => {
         , 800)
     }
     
-    // loopThroughComputerArray()
     
-    function lightRandomColor() {
-        const randomIndex = Math.ceil(Math.random() * 4)
-        if(randomIndex === 1){
-            toggleRedC()
-            computerArray.push(1)
-            console.log("computer Array" + computerArray)
-        }else if(randomIndex === 2){
-            toggleBlueC()
-            computerArray.push(2)
-            console.log(computerArray)
-        }else if(randomIndex === 3){
-            toggleYellowC()
-            computerArray.push(3)
-            console.log(computerArray)
-        }else if(randomIndex === 4){
-            toggleGreenC()
-            computerArray.push(4)
-            console.log(computerArray)
-        }
-    }
-    
-    setTimeout(lightRandomColor, 1000)
-    
-
-    
-    //Can rewrite computer functions into one with parameter
-    function toggleRedC() {
-        $('.red').toggleClass('redLit')
-         setTimeout(() => {
-             $('.red').toggleClass('redLit')
-         }, 400)
-         $('#ding').get(0).play()
-    }
-    
-    
-    
-    function toggleBlueC() {
-        $('.blue').toggleClass('blueLit')
-         setTimeout(() => {
-             $('.blue').toggleClass('blueLit')
-         }, 400)
-         $('#bleep').get(0).play()
-    }
-    
-    function toggleYellowC() {
-        $('.yellow').toggleClass('yellowLit')
-         setTimeout(() => {
-             $('.yellow').toggleClass('yellowLit')
-         }, 400)
-         $('#ping').get(0).play()
-    }
-    
-    function toggleGreenC() {
-        $('.green').toggleClass('greenLit')
-         setTimeout(() => {
-             $('.green').toggleClass('greenLit')
-         }, 400)
-         $('#beep').get(0).play()
-    }
-    
-    $('.red').on('click', () => {
-        $('#ding').get(0).play()
-    })
-    $('.yellow').on('click', () => {
-        $('#ping').get(0).play()
-    })
-    $('.green').on('click', () => {
-        $('#beep').get(0).play()
-    })
-    $('.blue').on('click', () => {
-        $('#bleep').get(0).play()
-    })
-
-
-
-
     function toggle (event, color, number) {
         $(event.target).toggleClass(color + 'Lit')
         setTimeout(() => {
@@ -147,18 +79,17 @@ $(()  => {
         }
     }
 
-    
-    
-
-    
     $('.red').on('click', () => toggle(event, 'red', 1))
     $('.blue').on('click', () => toggle(event, 'blue', 2))
     $('.yellow').on('click', () => toggle(event,'yellow', 3))
     $('.green').on('click', () => toggle(event,'green', 4))
     
-    
-    
-    
-    
-    
+    $('.red').on('click', () => $('#ding').get(0).play())
+    $('.yellow').on('click', () => $('#ping').get(0).play())
+    $('.green').on('click', () => $('#beep').get(0).play())
+    $('.blue').on('click', () => $('#bleep').get(0).play())
+
+
+    setTimeout(lightRandomColor, 1000)
+     
     })
